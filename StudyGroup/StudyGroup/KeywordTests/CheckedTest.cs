169cb9f76b2c0e705158d6b5fe61e6823cf2a06f
mheyner <mheyner@gmail.com>
2018-04-11 07:01:55 -0400

    Added checking test

---
+++ StudyGroup/StudyGroup/KeywordTests/CheckedTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGroup.KeywordTests
+{
+    public class CheckedTest
+    {
+        public int AccountTotalNotChecking(int newDeposits, bool notChecked = false)
+        {
+            int currentAccountValue = 2147483647;
+            currentAccountValue += newDeposits;
+            return currentAccountValue;
+        }
+
+        public int AccountTotalChecking(int newDeposits)
+        {
+            int currentAccountValue = 2147483647;
+
+            checked
+            {
+                currentAccountValue += newDeposits;
+            }
+
+            return currentAccountValue;
+        }
+    }
+}
--- StudyGroup/StudyGroup/MainTestingClass.cs
+++ StudyGroup/StudyGroup/MainTestingClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using InternalTest;
+using StudyGroup.KeywordTests;
 
 namespace StudyGroup
 {
@@ -7,55 +8,51 @@ namespace StudyGroup
     {
         public MainTestingClass()
         {
-            // empty constructor
+
         }
 
         static void Main(string[] args)
         {
             MainTestingClass interfaceAbstractMethod = new MainTestingClass();
 
-            // Internal tests
             // InternalTestClass internalTest = new InternalTestClass(); ** Holds the internal keyword and cannot be accessed since it's outside of the assembly **
             InternalTestWithinDLLClass internalTest = new InternalTestWithinDLLClass();
             internalTest.InternalTestClassMethod(); // ** Works because it is instantiated within another class which is not internal **
 
-            // Abstract tests
             // var abstractmethods = new AbstractTest(); Cannot instantiate Abstract classes or interfaces. They are static by default
             interfaceAbstractMethod.AbstractVoidMethodTesting();
             Console.WriteLine(AbstractStringMethodTesting());
 
-            // Sealed tests
             SealedTest sealedMethod = new SealedTest(); // Sealed can be instantiated but not inherited
             Console.WriteLine(sealedMethod.SealedMethodTest());
 
-            // Interfaced tests
             Console.WriteLine(interfaceAbstractMethod.InterfaceMethodStringTesting());
             interfaceAbstractMethod.InterfaceVoidMethodTesting("Void");
 
-            //Inheritance tests
             InheritanceTest inheritanceTest = new InheritanceTest();
-        }
 
-        // Overridden virtual abstract method
+            CheckedTest checking = new CheckedTest();
+            Console.WriteLine(checking.AccountTotalNotChecking(25, false));
+
+            // This will throw a runtime error because it's enclosed by the "checked" keyword 
+            Console.WriteLine(checking.AccountTotalChecking(25));
+        }
+        
         public override void AbstractVoidMethodTesting() 
         {
             base.AbstractVoidMethodTesting();
         }
-
-        // Interface implementation (the contact)
+        
         public string InterfaceMethodStringTesting()
         {
             return "This is the Interface method!";
         }
-
-        // Interface implementation (the contact)
+        
         public void InterfaceVoidMethodTesting(string methodName)
         {
             Console.WriteLine($"This is {methodName} method!");
         }
-
-        // Interface implementation (the contact)
-        // Cannot make this static because an Interface is a contract not an implementation
+        
         public void InterfaceStaticMethodTesting(string methodName) 
         {
             Console.WriteLine($"This is {methodName} method!");
