23eb08e0aace9de828b1a05b1d4fc1d6e54e143e
Lee Ames <lee.ames@silvertech.com>
2018-05-09 11:54:23 -0400

    adding Lee's (empty) project

---
+++ Lees_Assignments/Lee_StudyGroup/Lee_StudyGroup/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lee_StudyGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
